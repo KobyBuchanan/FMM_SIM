479b46b280e627ab9c951e82086cb32ecf43128a
KobyBuchanan <kobya.buchanan@gmail.com>
2026-08-08 12:30:23 -0400

    basic object struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 
+struct Object {
+    sf::Vector2f position;
+    sf::Color color;
+    float radius = 15.f;
+
+    Object() = default;
+    Object(sf::Vector2f m_position, sf::Color m_color)
+        : position{m_position}
+        , color{m_color} 
+        {}
+
+    };
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}),"SFML window", sf::Style::Titlebar | sf::Style::Close);
- 
+    
+    std::vector<Object> Objects;
+
+    for (int i = 0; i < 5; i++) {
+        sf::Vector2f pos = sf::Vector2f(400.f +(25*i), 300.f - (25*i));
+        sf::Color color = sf::Color::Red;
+        Object o{pos,color};
+        Objects.emplace_back(o);
+    }
+
+    sf::CircleShape templateCircle;
  
     //Main loop
     while (window.isOpen())
@@ -20,14 +43,15 @@ int main()
         // Clear screen
         window.clear();
 
-        // set the shape
-
-        sf::CircleShape shape(50.f);
-        shape.setFillColor(sf::Color(100, 250, 50));
-        shape.setPosition(sf::Vector2f(400,300));
-        window.draw(shape);
- 
+        for (const auto& obj : Objects) {
+            templateCircle.setRadius(obj.radius);
+            templateCircle.setFillColor(obj.color);
+            templateCircle.setPosition(obj.position);
+        
+        window.draw(templateCircle);
+    }
         // Update the window
         window.display();
     }
+    return 0;
 }
